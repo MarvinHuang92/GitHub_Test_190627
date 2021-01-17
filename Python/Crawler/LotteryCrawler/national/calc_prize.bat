84a7ab3545003690a0e58b1d7157900d4efd047d
Marvin at Home <marvinhuang@qq.com>
2021-01-17 22:59:12 +0800

    add national loterry calculation - stage 1

---
+++ Python/Crawler/LotteryCrawler/national/calc_prize.bat
@@ -0,0 +1,5 @@
+@echo off
+
+"D:\Program Files\Python36\python.exe" calc_prize.py
+
+pause
---
+++ Python/Crawler/LotteryCrawler/national/lottery_config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# 最早的一期开奖网页
+# 2019年8月1日，第19118期
+default_url = '/Article/news/group_id/3/article_id/88989.html'
+# 2020年8月11日，第20100期
+# default_url = '/Article/news/group_id/3/article_id/91912.html'
+
+# 默认自选号码
+default_array = [2, 0, 2, 1, 7, 0, 9]
