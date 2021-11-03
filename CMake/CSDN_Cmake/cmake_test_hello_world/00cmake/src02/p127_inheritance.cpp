99780e6006720bc1e37188180800d999d1da35c2
Marvin Huang <marvinhuang@qq.com>
2021-11-03 15:40:35 +0800

    sync status between office and home - 20211103_a

--- CMake/CSDN_Cmake/cmake_test_hello_world/00cmake/src02/p127_inheritance.cpp
+++ CMake/CSDN_Cmake/cmake_test_hello_world/00cmake/src02/p127_inheritance.cpp
@@ -11,9 +11,46 @@ using namespace std;
 
 // P127 继承 - 基本语法
 
+/*
+有三种继承方式：
+继承方式         父类的内容       子类的内容        父类权限的前两项在子类中不变，父类的private在子类中不可访问
+public          public          public
+                protected       protected
+                private         不可访问
+
+继承方式         父类的内容       子类的内容        父类权限的前两项在子类中变成 protected，父类的private在子类中不可访问
+protected       public          protected
+                protected       protected
+                private         不可访问
+
+继承方式         父类的内容       子类的内容        父类权限的前两项在子类中变成 private，父类的private在子类中不可访问
+private         public          private
+                protected       private
+                private         不可访问
+*/
+
+// 父类/基类
+class BaseClass
+{
+public:
+    int m_base_num;
+};
+
+// 子类/派生类
+class SubClass : public BaseClass // 用：表示继承即可，后面的 public 称为【继承方式】
+{
+public:
+    int m_sub_num;
+};
+
 void test_127_00()
 {
-    
+    SubClass sc;
+    sc.m_base_num = 0;
+    sc.m_sub_num = 1;
+
+    cout << sc.m_base_num << endl;
+    cout << sc.m_sub_num << endl;
 }
 
 
