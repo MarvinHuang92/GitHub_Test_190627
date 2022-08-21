17dccc0b40f6e30c25f79dfd3b7109ecba3e8dbd
Marvin at Home <marvinhuang@qq.com>
2022-08-21 23:26:40 +0800

    add a python 3.6 into repo for updating libraries.

---
+++ Python36/Scripts/can_viewer.py
@@ -0,0 +1,14 @@
+#!D:\Program Files\Python36\python.exe
+# coding: utf-8
+
+"""
+See :mod:`can.viewer`.
+"""
+
+from __future__ import absolute_import
+
+from can.viewer import main
+
+
+if __name__ == "__main__":
+    main()
